3b897519eb9eda693e9eb6c667996d63fb2b71f6
xliang01 <xliang01@gmail.com>
2022-01-27 01:43:35 -0800

    Medium: Longest unique substring

--- Medium/longest_unique_substring.py
+++ Medium/longest_unique_substring.py
@@ -24,7 +24,61 @@ class SolutionNaive (Solution):
                 return False
         return True
 
+class SolutionSlidingWindow (Solution):
+     def lengthOfLongestSubstring(self, s: str) -> int:
+        ascii_chars = [0] * 128
+        left = right = 0
+        longest = 0
+
+        # Expand the window to the right
+        while right < len(s):
+            # Store the character in the Set
+            ascii_chars[ord(s[right])] += 1
+            # If a duplicate character is found, collapse the window 
+            # and remove all found letters until the Set is cleared
+            while ascii_chars[ord(s[right])] > 1:
+                ascii_chars[ord(s[left])] -= 1
+                left += 1
+
+            right += 1
+            longest = max(longest, right - left)
+            
+        return longest
+
+
+class SolutionOptimalSlidingWindow (Solution):
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_position = {}
+        left = right = 0
+        longest = 0
+
+        for right in range(len(s)):
+            ascii = ord(s[right])
+
+            # If the character was already found, find the next index
+            # of that character to change the window bounds
+            if ascii in char_position:
+                left = max(char_position[ascii], left)
+
+            char_position[ascii] = right + 1
+            longest = max(longest, right - left + 1)
+
+        return longest
+            
+
 s: Solution = SolutionNaive()
+print(s.lengthOfLongestSubstring("abcabcbb"))
+print(s.lengthOfLongestSubstring("bbbbb"))
+print(s.lengthOfLongestSubstring("pwwkew"))
+print(s.lengthOfLongestSubstring("a"))
+
+s: Solution = SolutionSlidingWindow()
+print(s.lengthOfLongestSubstring("abcabcbb"))
+print(s.lengthOfLongestSubstring("bbbbb"))
+print(s.lengthOfLongestSubstring("pwwkew"))
+print(s.lengthOfLongestSubstring("a"))
+
+s: Solution = SolutionOptimalSlidingWindow()
 print(s.lengthOfLongestSubstring("abcabcbb"))
 print(s.lengthOfLongestSubstring("bbbbb"))
 print(s.lengthOfLongestSubstring("pwwkew"))
