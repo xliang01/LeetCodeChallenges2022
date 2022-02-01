c0b48d1e6d089edc59c95bedb397e8d785e97414
xliang01 <xliang01@gmail.com>
2022-01-31 23:58:17 -0800

    Easy: Max wealth

---
+++ Easy/maximum_wealth.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return 0
+
+class SolutionIterative (Solution):
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_amount = 0
+        for account in accounts:
+            max_amount = max(max_amount, self.accountTotal(account))
+        return max_amount
+
+    def accountTotal(self, account: List[int]) -> int:
+        sum = 0
+        for item in account:
+            sum += item
+        return sum
+
+
+s = SolutionIterative()
+print(s.maximumWealth([[1,2,3],[3,2,1]]))
